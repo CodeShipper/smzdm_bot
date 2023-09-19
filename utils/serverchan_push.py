de0e6d59e5712e90d9c4aafbd9ebb4543fe3b6ef
CodeShipper <qianwenzhi1989@hotmail.com>
2023-09-19 10:21:11 +0800

    Update serverchan_push.py

--- utils/serverchan_push.py
+++ utils/serverchan_push.py
@@ -13,11 +13,11 @@ def push_to_wechat(text,desp,secretKey):
     """
     url = f'http://sctapi.ftqq.com/{secretKey}.send'
     session = requests.Session()
-    data = {'text':text,'desp':desp}
+    data = {'title':text,'desp':desp}
     resp = session.post(url,data = data)
     return resp.json()
 
 
 if __name__ == '__main__':
-    resp = push_to_wechat(text = 'test', desp='hi', secretKey= config.SERVERCHAN_SECRETKEY)
+    resp = push_to_wechat(title = 'test', desp='hi', secretKey= config.SERVERCHAN_SECRETKEY)
     print(resp)
